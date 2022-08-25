62c9608549a4962f8e3e4ba6227f2a3e96a6d333
IvanArmella <ivancraftero@gmail.com>
2022-08-24 21:31:59 -0300

    Cambio a PQT5

--- TP1/TP1.py
+++ TP1/TP1.py
@@ -1,51 +1,52 @@
-from tkinter import *
-from tkinter import filedialog
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 18 02:13:24 2022
+
+@author: Ivan
+"""
+import sys
+from PyQt5 import QtWidgets,QtGui,QtCore
+from interfaz import Ui_TP1
 import numpy as np 
 import matplotlib.pyplot as plt
 import imageio.v2 as imageio
-from time import time
 
-class Interfaz:
-    def __init__(self,ventana):
-        self.ventana=ventana
-        self.ventana.title("Procesamiento de Imagenes")
+class Programa(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Programa, self).__init__()
+        self.interfaz=Ui_TP1()
+        self.interfaz.setupUi(self)
+        self.validadorFloat(self.interfaz.edtLuminancia)
+        self.validadorFloat(self.interfaz.edtSaturacion)
+        self.interfaz.lblImagen.setPixmap(QtGui.QPixmap("image2.png"))
+        self.interfaz.btnSeleccionar.clicked.connect(self.seleccionar)
+        self.interfaz.btnActualizar.clicked.connect(self.actualizar)
         self.ruta="image2.png"
-        self.imagen=PhotoImage(file=self.ruta)
-        self.lblImagen=Label(ventana,image=self.imagen,padx=5,pady=5)
-        self.btnSeleccionar=Button(ventana,text="Selecconar",command=self.seleccionar,padx=5,pady=5)
-        self.btnActualizar=Button(ventana,text="Actualizar",command=self.actualizar,padx=5,pady=5)
-        self.lblSaturacion=Label(ventana,text="Saturación",padx=5,pady=5)
-        self.lblLuminancia=Label(ventana,text="Luminancia",padx=5,pady=5)
-        self.edtSaturacion=Entry(ventana)
-        self.edtLuminancia=Entry(ventana)
-        self.edtSaturacion.insert(0,"1")
-        self.edtLuminancia.insert(0,"1")
-        self.lblImagen.grid(row=1,columnspan=4)
-        self.lblLuminancia.grid(row=2,column=0)
-        self.edtLuminancia.grid(row=2,column=1)
-        self.lblSaturacion.grid(row=2,column=2)
-        self.edtSaturacion.grid(row=2,column=3)
-        self.btnSeleccionar.grid(row=3,column=2)
-        self.btnActualizar.grid(row=3,column=3)
-
+        self.show()
+        
     def seleccionar(self):
-        ruta=filedialog.askopenfilename(title="Seleccionar Imagen",filetypes=[("Imagenes", ".png")])
-        if ruta[-4:]==".png":
-            self.ruta=ruta
-            self.imagen=PhotoImage(file=ruta)
-            self.lblImagen=Label(ventana,image=self.imagen,padx=5,pady=5)
-            self.lblImagen.grid(row=1,columnspan=4)
-            self.edtSaturacion.delete(0, END)
-            self.edtLuminancia.delete(0, END)
-            self.edtSaturacion.insert(0,"1")
-            self.edtLuminancia.insert(0,"1")
+        ruta=QtWidgets.QFileDialog(self).getOpenFileName(caption="Seleccionar imagen",filter="PNG(*.png)")
+        if ruta[0][-4:]==".png":
+            self.ruta=ruta[0]
+            self.interfaz.lblImagen.setPixmap(QtGui.QPixmap(ruta[0]))
+            self.interfaz.edtLuminancia.setText("1")
+            self.interfaz.edtSaturacion.setText("1")
+        
+    def reemplazar(self,obj):
+        if ',' in obj.text():
+            obj.setText(obj.text().replace(",","."))
+
+    def validadorFloat(self,obj):
+        exp  = QtCore.QRegExp("[0-9]+((\.|\,)[0-9]+)?")
+        obj.setValidator(QtGui.QRegExpValidator(exp));
+        obj.textChanged.connect(lambda: self.reemplazar(obj))
         
     def actualizar(self):
         fRGB=np.array([0.299,0.587,0.114,0.595716,-0.274453,-0.321263,0.211456,-0.522591,0.311135]).reshape(3,3)
         fYIQ=np.array([1,0.9663,0.6210,1,-0.2721,-0.6474,1,-1.1070,1.7046]).reshape(3,3)
         im=imageio.imread(self.ruta)/255
-        a=float(self.edtLuminancia.get())
-        b=float(self.edtSaturacion.get())
+        a=float(self.interfaz.edtLuminancia.text())
+        b=float(self.interfaz.edtSaturacion.text())
         f=im.shape[0]
         c=im.shape[1]
         for i in range(f):      
@@ -57,10 +58,9 @@ class Interfaz:
         im[:,:,:]=np.clip(im[:,:,:]*255,0,255)
         im=im.astype(np.uint8)
         imageio.imwrite('a.png',im)
-        self.imagen=PhotoImage(file="a.png")
-        self.lblImagen=Label(ventana,image=self.imagen,padx=5,pady=5)
-        self.lblImagen.grid(row=1,columnspan=4)
-
-ventana=Tk()
-programa=Interfaz(ventana)
-ventana.mainloop()
+        self.interfaz.lblImagen.setPixmap(QtGui.QPixmap("a.png"))
+        
+if __name__ == "__main__": 
+    app=QtWidgets.QApplication(sys.argv)
+    programa=Programa()
+    sys.exit(app.exec())
